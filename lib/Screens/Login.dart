947e760b4c3ef0c6833d635acd509d09a215ab05
Heyflutter <vsakinal@gmail..com>
2023-02-24 16:55:21 +0530

    BeforeError

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,6 +1,12 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import '../Components/textfield.dart';
 import '../Constants.dart' as constant;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'mainscreen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,43 +16,69 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late String email, password;
+  int myvar = 0;
+
   @override
   Widget build(BuildContext context) {
+    final _auth = FirebaseAuth.instance;
     return Scaffold(
       backgroundColor: constant.textPrimary,
-      body:  Column(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-
-          Expanded(child: Image.network('https://t4.ftcdn.net/jpg/00/31/33/13/240_F_31331324_bqXgqwmlnnXaOeXwFv8CrO6oMHpAKPum.jpg')),
-          textfield(text: 'Email',isPassword: false,),
-          textfield(text: 'Password',isPassword: true,),
+          Expanded(
+              child: Image.network(
+                  'https://t4.ftcdn.net/jpg/00/31/33/13/240_F_31331324_bqXgqwmlnnXaOeXwFv8CrO6oMHpAKPum.jpg')),
+          textfield(
+            text: 'Email',
+            isPassword: false,
+            onChanged: (value) {
+              email = value;
+            },
+          ),
+          textfield(
+            text: 'Password',
+            isPassword: true,
+            onChanged: (value) {
+              password = value;
+            },
+          ),
           ClipRRect(
             borderRadius: BorderRadius.circular(40.0),
             child: Container(
               width: 180,
               color: constant.mailColor,
               child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40.0),
-                    side: BorderSide(
-                      width: 2,
-                      color: constant.mailColor
-                    )
-                  )
-                ),
-                  onPressed: (){}, child: Text('Login',
-              style: TextStyle(
-                fontSize: 40.0,
-                color: constant.textPrimary
-              ),)),
+                  style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40.0),
+                          side:
+                              BorderSide(width: 2, color: constant.mailColor))),
+                  onPressed: () async {
+                    try {
+                      final newUser = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+
+                      if (newUser.user != null && myvar != 0) {
+                        Navigator.pushReplacement(
+                            context, MaterialPageRoute(builder: (context) => MainScreen()));
+
+                      }
+                    } catch (e) {
+                      debugPrint('$e');
+                    }
+                  },
+                  child: Text(
+                    'Login',
+                    style:
+                        TextStyle(fontSize: 40.0, color: constant.textPrimary),
+                  )),
             ),
           ),
           Expanded(child: Container())
-
-       ],
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:test2/Screens/Login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
